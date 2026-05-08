e9ece60bfbc56306ce1337650eebee16bbba6c63
Amalia Zarari <zar.amalia2@hotmail.com>
2026-05-08 22:25:11 +0300

    18-04-2026 - Classes (User & Activity)
    - Def (Edit time etc)
    
    28-04-2026
    - Αλλαγές στη main (ερωτήσεις για τον χρήστη)
    - αρχείο με ονόματα χρηστών προς εισαγωγή
    
    02-05-2026
     - Έκανα αλλαγές για την εισαγωγή και αποθήκευση των χρηστών.
     - Δημιούργησα μέθοδο fileMethods pickle
    
     06-05-2026
     Έκανα ένα απλό παράθυρο με τίτλο
    
     08-05-2026
     Νέο αρχείο GUI & επανέφερα fileMethods pickle

--- src/application.py
+++ src/application.py
@@ -1,11 +1,11 @@
 from classes.user import User
 from classes.activity import Hobbie,Task
+from methods.fileMethods import *
 
 #τεστ ότι δουλεύουν οι κλάσεις και οι συναρτήσεις
 def app():
     user1 = User("Amalia", "std170663@ac.eap.gr", "pass123$", 8, 8)
     print(user1)
-    print("hello")
 
     hobbie1 = Hobbie(2, 5, "tennis")
     print(hobbie1)
@@ -19,12 +19,12 @@ def app():
     task1.edit_priority(7)
     print(task1)
 
-print("test")
-
 # --- Κύριο Μενού ---
 def main():
     # Η τοπική λίστα που θα κρατάει τα δεδομένα μας κατά την εκτέλεση
-    read_log_file = 'user_names_log.csv'
+    read_log_file = 'usersDB.txt'
+    users = read_users(read_log_file)
+    print(users)
 
     while True:
         print("\n=== MENOY ΔΙΑΧΕΙΡΙΣΗΣ ΧΡΟΝΟΥ ===")
@@ -39,25 +39,24 @@ def main():
                 name = input("Όνομα: ")
                 email = input("email: ")
                 password = input("password: ")
-                our_spend_activities = input("Εκτιμώμενος χρόνος υποχρεώσεων: ")
-                our_spend_hobbies = input("Εκτιμώμενος χρόνος δραστηριοτήτων: ")
-                read_log_file.add_new_user(new_user)
+                our_spend_activities = input("Εκτιμώμενος χρόνος υποχρεώσεων σε ώρες: ")
+                our_spend_hobbies = input("Εκτιμώμενος χρόνος δραστηριοτήτων σε ώρες: ")
+                # προσθήκη νέου χρήστη στήν λίστα.
+                users.append(User(name, email, password, our_spend_activities, our_spend_hobbies))
                 print("Συγχαρητήρια!\nΝέος χρήστης προστέθηκε με επιτυχία")
             elif choice == 'b':
                 email = input("email: ")
                 password = input("password: ")
-                #read_log_file.add_new_user(new_user) να γίνει αλλαγή σε διάβασμα δεδομένων
                 print("Συνδεθήκατε")
             else:
-                print ("Ops!\nΠληκτρολόγησες δεδομένα :( \nΞαναπροσπάθησε")
+                print("Ops!\nΠληκτρολόγησες δεδομένα :( \nΞαναπροσπάθησε")
 
         elif choice == '2':
+            write_users("usersDB.txt", users)
             print("Έξοδος...")
             break
         else:
             print("Λάθος επιλογή, προσπαθήστε ξανά.")
-
-        choice = input("Επιλογή: ")
 
 if __name__ == "__main__":
     app() #τεστ ότι δουλεύουν οι κλάσεις και οι συναρτήσεις
